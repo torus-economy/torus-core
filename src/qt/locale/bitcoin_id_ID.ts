09fe2823fb2aa463ab009c830e9726543095d00f
noise23 <admin@acidpool.com>
2016-01-24 18:23:58 +0200

    Copyright year

--- src/qt/locale/bitcoin_id_ID.ts
+++ src/qt/locale/bitcoin_id_ID.ts
@@ -15,7 +15,7 @@
         <location line="+41"/>
         <source>Copyright © 2009-2014 The Bitcoin developers
 Copyright © 2012-2014 The NovaCoin developers
-Copyright © 2014 The SHROOMS developers</source>
+Copyright © 2015-2016 The SHROOMS developers</source>
         <translation>Copyright © 2009-2014 para pengembang Bitcoin
 Copyright © 2012-2014 para pengembang NovaCoin
 Copyright © 2014 para pengembang SHROOMS</translation>
