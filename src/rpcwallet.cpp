10561cb19e3f2fa836a85fc77e8873ac16e7b358
noise23 <admin@acidpool.com>
2016-01-28 20:20:13 +0200

    Difficulty RPC compatible with Insight explorer

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -108,8 +108,8 @@ Value getinfo(const Array& params, bool fHelp)
     obj.push_back(Pair("proxy",         (proxy.first.IsValid() ? proxy.first.ToStringIPPort() : string())));
     obj.push_back(Pair("ip",            addrSeenByPeer.ToStringIP()));
 
-    diff.push_back(Pair("proof-of-work",  GetDifficulty()));
-    diff.push_back(Pair("proof-of-stake", GetDifficulty(GetLastBlockIndex(pindexBest, true))));
+    diff.push_back(Pair("proofofwork",  GetDifficulty()));
+    diff.push_back(Pair("proofofstake", GetDifficulty(GetLastBlockIndex(pindexBest, true))));
     obj.push_back(Pair("difficulty",    diff));
 
     obj.push_back(Pair("testnet",       fTestNet));
