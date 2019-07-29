f40c5dd1d6b316ba3105986b48b79207c41c894d
noise23 <admin@acidpool.com>
2019-07-30 02:34:29 +0300

    Update ip address for checkip.dyndns.org

--- src/net.cpp
+++ src/net.cpp
@@ -364,7 +364,7 @@ bool GetMyExternalIP(CNetAddr& ipRet)
         //  <?php echo $_SERVER["REMOTE_ADDR"]; ?>
         if (nHost == 1)
         {
-            addrConnect = CService("91.198.22.70",80); // checkip.dyndns.org
+            addrConnect = CService("216.146.43.71",80); // checkip.dyndns.org
 
             if (nLookup == 1)
             {
