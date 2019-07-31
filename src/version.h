778d38fb925b827798322f746bca4b507a477df8
noise23 <admin@acidpool.com>
2019-07-31 23:27:51 +0300

    v1.6 Hardfork: bump protocol version, reject old versions after fork.

--- src/version.h
+++ src/version.h
@@ -31,13 +31,14 @@ static const int DATABASE_VERSION = 70509;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 60123;
+static const int PROTOCOL_VERSION = 60124;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION = 60023;
+static const int MIN_FORK_PROTO_VERSION = 60124;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
@@ -46,6 +47,7 @@ static const int CADDR_TIME_VERSION = 31402;
 // only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 60002;
 static const int NOBLKS_VERSION_END = 60022;
+static const int NOBLKS_VERSION_END_FORK = 60123;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
