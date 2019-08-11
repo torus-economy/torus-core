7a74f3f075ec68b280c9a68d373462775ec4d7b8
noise23 <admin@acidpool.com>
2019-08-11 04:34:36 +0300

    Post-hardfork versioning cleanup
    Back to usual MIN_PEER_PROTO_VERSION and NOBLKS_VERSION_END code.

--- src/version.h
+++ src/version.h
@@ -37,8 +37,7 @@ static const int PROTOCOL_VERSION = 60124;
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 60023;
-static const int MIN_FORK_PROTO_VERSION = 60124;
+static const int MIN_PEER_PROTO_VERSION = 60124;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
@@ -46,8 +45,7 @@ static const int CADDR_TIME_VERSION = 31402;
 
 // only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 60002;
-static const int NOBLKS_VERSION_END = 60022;
-static const int NOBLKS_VERSION_END_FORK = 60123;
+static const int NOBLKS_VERSION_END = 60123;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
