42e512275c66d2c6929b86c03f365efca75f18e0
Sven Skender <sskender@pm.me>
2021-08-09 17:09:39 +0200

    Coin year reward changed to 5%

--- src/main.h
+++ src/main.h
@@ -40,7 +40,7 @@ static const unsigned int MAX_INV_SZ = 50000;
 static const int64_t MIN_TX_FEE = 1000000;
 static const int64_t MIN_RELAY_TX_FEE = MIN_TX_FEE;
 static const int64_t MAX_MONEY = 120000000 * COIN;
-static const int64_t COIN_YEAR_REWARD = 30 * CENT;
+static const int64_t COIN_YEAR_REWARD = 5 * CENT;
 
 inline bool MoneyRange(int64_t nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 // Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp.
