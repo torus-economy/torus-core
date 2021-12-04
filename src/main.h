966ad66e98956bd8ee15f9ac1b6c2970f751575d
Sven Skender <sskender@pm.me>
2021-12-04 13:45:33 +0100

    Premine 150M coins

--- src/main.h
+++ src/main.h
@@ -39,7 +39,7 @@ static const unsigned int MAX_ORPHAN_TRANSACTIONS = MAX_BLOCK_SIZE/100;
 static const unsigned int MAX_INV_SZ = 50000;
 static const int64_t MIN_TX_FEE = 1000000;
 static const int64_t MIN_RELAY_TX_FEE = MIN_TX_FEE;
-static const int64_t MAX_MONEY = 120000000 * COIN;
+static const int64_t MAX_MONEY = 150000000 * COIN;
 static const int64_t COIN_YEAR_REWARD = 5 * CENT;
 
 inline bool MoneyRange(int64_t nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
